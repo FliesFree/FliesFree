2404e432ec410d13fbdd25551bde85aa7e05443a
Pietro Rignanese <pietro_rignanese@hotmail.it>
2017-12-12 19:38:44 +0100

    Add files via upload

--- Programma_Prova1_Raspberry/cattura_immagine.py
+++ Programma_Prova1_Raspberry/cattura_immagine.py
@@ -7,9 +7,11 @@ import time
 import data_ora 
 
 def get_img():
+    print("Sto scattando una foto...")
     camera = PiCamera()
     camera.start_preview()
     camera.annotate_text = data_ora.data_ora_attuale()
     time.sleep(5)
     camera.capture('/home/pi/FliesFree/Prove/Prove_Python/Flies_Free_Prova1/Trappola/%s.jpg' % data_ora.data_ora_attuale())
     camera.stop_preview()
+    print("Foto scattata.")
